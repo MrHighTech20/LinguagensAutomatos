17845e9be24b3e0b843556329ff9c1ee9492c3f5
MrHighTech20 <marcoscantelli@hotmail.com>
2023-03-28 09:40:05 -0300

    colocando no

--- Aula5.c
+++ Aula5.c
@@ -41,6 +41,22 @@ Tabela* t;
 #define PARESQ 0
 #define PARDIR 1
 
+Tabela* cria(void){
+    return NULL;
+}
+
+Tabela* add(Tabela* t,Token *tok,int linha, int coluna){
+    Tabela* no = (Tabela*) malloc(sizeof(Tabela));
+    no->tipo = tok->tipo;
+    no->valor = tok->valor;
+    no->linha = linha;
+    no->coluna = coluna;
+    no->next = t;
+    return no;
+}
+
+// t = add(t,tok,linha,coluna);
+
 int main(void){
     FILE *arqFonte;
     FILE *tabSim;
@@ -57,7 +73,33 @@ int main(void){
     printf("Lendo Arquivo de entrada...\n\n");
 
     char entrada[200];
+    Token tok;
 
+    t=cria();
+
+    printf("\n\n********PROCESSANDO********\n\n");
+
+    fgets(entrada,200,arqFonte);
+
+    inicializa_analise(entrada);
+
+    print("Escrevendo Arquivos...");
+
+    while(proximo_token(&tok)!=NULL){
+        imprime_token(&tok,fluxoTokens);
+    }
+
+    //Impressao da Tabela de Simbolos
+
+    print(t,tabSim);
+
+    fclose(arqFonte);
+    fclose(tabSim);
+    fclose(fluxoTokens);
+
+    printf("\n\nAnalise Lexica finalizada com sucesso\n\n")
+
+    return 0;
 }
 
 
